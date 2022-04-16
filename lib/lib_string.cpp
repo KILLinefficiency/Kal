c2c1d4704df2bfde2b1fbb554b0119f9abb66896
KILLinefficiency <shreyas.sable2166@gmail.com>
2022-04-16 17:04:47 +0530

    added a basic string lib written from scratch

---
+++ lib/lib_string.cpp
@@ -0,0 +1,9 @@
+namespace lib {
+    bool ends_with(const std::string& major_string, const std::string& minor_string) {
+        int major_string_size = major_string.size();
+        int minor_string_size = minor_string.size();
+        int diff_size = major_string_size - minor_string_size;
+
+        return major_string.substr(diff_size, major_string_size - 1) == minor_string;
+    }
+}
