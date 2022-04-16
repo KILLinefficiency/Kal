4d6cd790b9538b2d5718f05165558bc807ab36e1
KILLinefficiency <shreyas.sable2166@gmail.com>
2022-04-16 17:16:09 +0530

    updated .gitignore

--- build.sh
+++ build.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
 function build() {
-    g++ kal.cpp -o kal
+    ! [ -d bin ] && mkdir bin
+    g++ kal.cpp -o bin/kal
+}
+
+function install() {
+    build
+    sudo cp bin/kal /usr/local/bin
 }
 
 [ "$1" == "build" ] && build
+[ "$1" == "install" ] && install
