dcd00c6f55f3d9102b6f18ded518306396a0b48e
KILLinefficiency <shreyas.sable2166@gmail.com>
2022-04-22 01:31:21 +0530

    added style and colors in style.hpp

---
+++ style.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+namespace style {
+    std::string reset      = "\u001b[0m";
+
+    std::string bold       = "\u001b[1m";
+    std::string underline  = "\u001b[4m";
+
+    std::string black      = "\u001b[30m";
+    std::string red        = "\u001b[31m";
+    std::string green      = "\u001b[32m";
+    std::string yellow     = "\u001b[33m";
+    std::string blue       = "\u001b[34m";
+    std::string magenta    = "\u001b[35m";
+    std::string cyan       = "\u001b[36m";
+    std::string white      = "\u001b[37m";
+}
