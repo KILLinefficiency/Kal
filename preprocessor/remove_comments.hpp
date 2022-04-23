1c18ad6227786e10b7352c3c4904f35bd9884ed4
KILLinefficiency <shreyas.sable2166@gmail.com>
2022-04-24 01:48:07 +0530

    minor changes

--- preprocessor/remove_comments.hpp
+++ preprocessor/remove_comments.hpp
@@ -1,22 +1,24 @@
 #pragma once
 
-std::string remove_comments(const std::string& line) {
-    if(line[0] == ';') {
-        return "";
-    }
-    
-    bool enable_removal = true;
-    int required_len = 0;
-    int line_size = line.size();
-    for(int char_index = 0; char_index < line_size; char_index++) {
-        if(line[char_index] == '"') {
-            enable_removal = !enable_removal;
+namespace preproc {
+    std::string remove_comments(const std::string& line) {
+        if(line == "" || line == ";" || line[0] == ';') {
+            return "";
         }
-        if((line[char_index] == ';') && enable_removal) {
-            break;
-        }
-        required_len++;
-    }
 
-    return line.substr(0, required_len);
+        bool enable_removal = true;
+        int required_len = 0;
+        int line_size = line.size();
+        for(int char_index = 0; char_index < line_size; char_index++) {
+            if(line[char_index] == '"') {
+                enable_removal = !enable_removal;
+            }
+            if((line[char_index] == ';') && enable_removal) {
+                break;
+            }
+            required_len++;
+        }
+
+        return line.substr(0, required_len);
+    }
 }
--- preprocessor/trim_spaces.hpp
+++ preprocessor/trim_spaces.hpp
@@ -1,16 +1,18 @@
 #pragma once
 
+namespace preproc {
 std::string trim_leading(const std::string& line) {
-    int size = line.size();
-    int white_space_len = 0;
-    for(int line_index = 0; line_index < size; line_index++) {
-        if((line[line_index] == ' ') || (line[line_index] == '\t')) {
-            white_space_len++;
-            continue;
+        int size = line.size();
+        int white_space_len = 0;
+        for(int line_index = 0; line_index < size; line_index++) {
+            if((line[line_index] == ' ') || (line[line_index] == '\t')) {
+                white_space_len++;
+                continue;
+            }
+            break;
         }
-        break;
-    }
 
-    int rest_str_len = size - white_space_len;
-    return line.substr(white_space_len, rest_str_len);
+        int rest_str_len = size - white_space_len;
+        return line.substr(white_space_len, rest_str_len);
+    }
 }
