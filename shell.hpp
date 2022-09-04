7f61d9d66ed2ef5b776cc0f1d2e5863d3e7d4138
KILLinefficiency <shreyas.sable2166@gmail.com>
2022-09-04 20:26:17 +0530

    made minor changes and made the multiline command thing in shell good looking

--- shell.hpp
+++ shell.hpp
@@ -16,6 +16,7 @@ namespace shell {
         while(true) {
             std::string command;
             std::vector<std::string> prog_args = {};
+            std::vector<std::vector<std::string>> tokens;
 
             std::cout << style::bold << "\nKal" << style::reset << " " << style::bold << style::green << "[" << count << "]:" << style::reset << " ";
             std::getline(std::cin, command);
@@ -37,21 +38,22 @@ namespace shell {
                 continue;
             }
 
-            if(command == ".edit") {
-                std::string secondary_prompt = style::bold + style::blue + "> " + style::reset;
+            if(command == ".multi") {
+                std::string multi_prompt = style::bold + style::blue + "> " + style::reset;
                 std::string edit_line;
                 std::vector<std::string> edit_lines;
-                std::cout << secondary_prompt;
+                std::cout << style::bold << style::blue << "\nIn:" << style::reset << "\n";
+                std::cout << multi_prompt;
                 while(std::getline(std::cin, edit_line) && edit_line != ".eof") {
-                    std::cout << secondary_prompt;
+                    std::cout << multi_prompt;
                     if(edit_line != "") {
                         edit_lines.emplace_back(edit_line);
                         count++;
                     }
                 }
-                count++;
-                std::cout << "\n";
-                std::vector<std::vector<std::string>> tokens = lexer::tokenize(edit_lines);
+
+                tokens = lexer::tokenize(edit_lines);
+                std::cout << style::bold << style::blue << "\nOut:" << style::reset << "\n";
                 line_exec(tokens, var, prog_args);
                 continue;
             }
@@ -60,13 +62,13 @@ namespace shell {
                 std::string file_name = command.substr(1);
                 std::vector<std::string> source_lines = preproc::initial_preprocessing(file_name);
                 preproc::preprocess(source_lines, init_file_path);
-                std::vector<std::vector<std::string>> tokens = lexer::tokenize(source_lines);
+                tokens = lexer::tokenize(source_lines);
                 line_exec(tokens, var, prog_args);
                 count++;
                 continue;
             }
 
-            std::vector<std::vector<std::string>> tokens = { lib::split(command, ' ') };
+            tokens = { lib::split(command, ' ') };
             line_exec(tokens, var, prog_args);
 
             count++;
