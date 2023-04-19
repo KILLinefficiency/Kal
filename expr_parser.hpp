1292f575c9162132847e0680c1d49888304b2dbb
KILLinefficiency <shreyas.sable2166@gmail.com>
2023-04-19 19:32:31 +0530

    Re-wrote some code. Now it works recursively for the evaluation of nested expressions.

--- expr_parser.hpp
+++ expr_parser.hpp
@@ -1,48 +1,43 @@
-#pragma once
-
 #include <queue>
 #include <deque>
 #include <iostream>
 
-double eval_expression(std::string expr) {
+double eval(std::string& expr, int& index) {
     double result = 0;
-    expr += ' ';
-
     std::deque<double> numbers;
     std::queue<char> symbols;
 
-    int start = 0;
-    int current = 0;
-    int len = 0;
-    int expr_len = expr.size();
-    for(int tok = 0; tok < expr_len; tok++) {
-        if(expr[tok] == '+' || expr[tok] == '-' || expr[tok] == '*' || expr[tok] == '/') {
-            symbols.push(expr[tok]);
+    index++;
+    while(expr[index - 1] != ')') {
+        if(expr[index] == '(') {
+            double nested_value = eval(expr, index);
+            numbers.push_back(nested_value);
+            index++;
+            continue;
         }
-        if((expr[tok] >= '0' && expr[tok] <= '9') || expr[tok] == '.') {
-            len++;
+        if(expr[index] == '+' || expr[index] == '-' || expr[index] == '*' || expr[index] == '/') {
+            symbols.push(expr[index]);
         }
-        else {
-            std::string value = expr.substr(start, len);
-            start = current + 1;
-            len = 0;
-            if(value == "") {
-                current++;
-                continue;
+        if(expr[index] >= '0' && expr[index] <= '9' || expr[index] == '.') {
+            int begin = index;
+            while(expr[index] >= '0' && expr[index] <= '9' || expr[index] == '.') {
+                index++;
             }
+            std::string value = expr.substr(begin, index - begin);
             numbers.push_back(std::stod(value));
         }
-        current++;
+        index++;
     }
 
-    while(numbers.size() != 1) {
+    while(numbers.size() - 1) {
         double x = numbers.front();
         numbers.pop_front();
         double y = numbers.front();
         numbers.pop_front();
-        char oper = symbols.front();
+        char op = symbols.front();
         symbols.pop();
-        switch(oper) {
+
+        switch(op) {
             case '+':
                 numbers.push_front(x + y);
                 break;
@@ -62,4 +57,13 @@ double eval_expression(std::string expr) {
     numbers.pop_front();
 
     return result;
+}
+
+int main() {
+    std::string expr = "(1 + (2 * 3 / (2 + (0.5 + 0.5))))";
+    expr = "((2 * 2) + (5 * 2))";
+    int index = 0;
+    double x = eval(expr, index);
+    std::cout << expr << " = " << x << std::endl;
+    return 0;
 }
