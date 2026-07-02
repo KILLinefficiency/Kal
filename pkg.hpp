88731013d26b2f23ee96d9dc87d69bab477ad8aa
KILLinefficiency <shreyas.sable2166@gmail.com>
2026-07-03 03:03:28 +0530

    add: added basic beginning code for the package manager

---
+++ pkg.hpp
@@ -0,0 +1,107 @@
+#pragma once
+
+#include <cstdlib>
+#include <sstream>
+
+const std::string GIT = "git";
+const char* KAL_PKG = std::getenv("KAL_PKG");
+
+namespace pkg {
+    bool check() {
+        std::string git_cmd = GIT + " > /dev/null 2>&1";
+        return std::system(git_cmd.c_str()) == 256;
+    }
+
+    bool valid_char(char c) {
+        return (c >= 'a' && c <= 'z') ||
+            (c >= 'A' && c <= 'Z') ||
+            (c >= '0' && c <= '9') ||
+            (c == '/' || c == ':' || c == '.' || c == '@');
+    }
+
+    std::string prepare_url(std::string pkg) {
+        int index = 0;
+        int size = pkg.size();
+        int slash_count = 0;
+
+        std::string url;
+
+        while(index < size) {
+            if(index == 0 && (size > 8) && pkg.substr(index, 8) == "https://") {
+                while(index < size) {
+                    if(!valid_char(pkg[index])) {
+                        // ERR:
+                        std::cerr << "Invalid Package Name!\n";
+                        exit(1);
+                    }
+                    index++;
+                }
+                url = pkg;
+                break;
+            }
+
+            if(!valid_char(pkg[index])) {
+                // ERR:
+                std::cerr << "Invalid Package Name!\n";
+                exit(1);
+            }
+
+            if(pkg[index] == '/') {
+                slash_count++;
+            }
+
+            index++;
+        }
+
+        if(slash_count == 1) {
+            url = "https://www.github.com/" + pkg;
+        }
+
+        return url;
+    }
+
+    std::string get_pkg_name(std::string pkg_label) {
+        int end = pkg_label.size();
+        int last = end - 1;
+
+        std::string pkg_name = "";
+        if(last > 0) {
+            while(last >= 0 && pkg_label[last] != '/') {
+                last--;
+            }
+
+            pkg_name = pkg_label.substr(last + 1, end - last);
+        }
+
+        return pkg_name;
+    }
+
+    void fetch(std::string pkg_label) {
+        if(!KAL_PKG) {
+            // ERR:
+            std::cerr << "KAL_PKG not set!\n";
+            exit(1);
+        }
+
+        if(!check()) {
+            // ERR:
+            std::cerr << "Git needed!\n";
+            exit(1);
+        }
+
+        std::string pkg_url = prepare_url(pkg_label);
+
+        // TODO: Create KAL_PKG directory if it doesn't exist.
+
+        std::stringstream cmd;
+        cmd << GIT << " "
+            << "clone "
+            << "--depth=1 "
+            << pkg_url << " "
+            << KAL_PKG << "/"
+            << get_pkg_name(pkg_label) << " "
+            << "> /dev/null 2>&1";
+        
+        std::cout << "[CMD]: " << cmd.str() << "\n";
+    }
+}
